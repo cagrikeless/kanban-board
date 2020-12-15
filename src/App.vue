159b6e60342a9128be5379b5355dc021a8380e04
cagrikeless <cagrikeless@github.com>
2020-12-15 13:45:31 +0300

    Repo init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col form-inline">
+        <b-form-input v-model="newTask" id="taskBox" placeholder="Enter Task" style="margin-bottom:15px" @keyup.enter="add"></b-form-input>
+         <b-button class="ml-2" style="margin-bottom: 15px" variant="primary" @click="add">Add</b-button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="p-2 alert alert-secondary">
+          <h3>Backlog</h3>
+          <draggable class="list-group" :list="arrBacklog" group="tasks">
+            <div class="list-group-item" v-for="element in arrBacklog" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-primary">
+          <h3>In Progress</h3>
+          <draggable class="list-group" :list="arrInProgress" group="tasks">
+            <div class="list-group-item" v-for="element in arrInProgress" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-warning">
+          <h3>Tested</h3>
+          <draggable class="list-group" :list="arrTested" group="tasks">
+            <div class="list-group-item" v-for="element in arrTested" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-success">
+          <h3>Done</h3>
+          <draggable class="list-group" :list="arrDone" group="tasks">
+            <div class="list-group-item" v-for="element in arrDone" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import draggable from 'vuedraggable'
+import main from './main'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  draggable
+  },
+  data() {
+    return {
+      newTask: "",
+      arrBacklog: [
+        {name: "Code sign up page"},
+        {name: "Test dashboard"},
+        {name: "Style Registeration"},
+        {name: "say help with designs"}
+      ],
+      arrInProgress: [],
+      arrTested: [],
+      arrDone:[]
+
+    }
+  },
+  methods: {
+    add() {
+        if(this.newTask) {
+        this.arrBacklog.push({name:this.newTask});
+      }
+      main.db.collection('boards').add({
+        newTask: this.newTask
+      })
+      .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function(error) {
+    console.error("Error adding document: ", error);
+      });
+      
+    },
+    delete() {
+
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import firebase from 'firebase'
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAf1skWH4ZjefFNv-rNepkt9vlx2OSHE4I",
+    authDomain: "kanban-db-c8768.firebaseapp.com",
+    projectId: "kanban-db-c8768",
+    storageBucket: "kanban-db-c8768.appspot.com",
+    messagingSenderId: "251429112054",
+    appId: "1:251429112054:web:b158cc02d24a017f6604d7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  require("firebase/firestore");
+  var db = firebase.firestore();
+
+  // DB SETTINGS in firebase
+  db.settings({
+    timestampsInSnapshots: true
+  });
 
 Vue.config.productionTip = false
+
+import { BootstrapVue , IconsPlugin} from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   render: h => h(App),
